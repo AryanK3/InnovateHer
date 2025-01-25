3be36dbaa9350e5bf723f8bbd20c7a34c905554d
AryanK3 <aryankiran136@gmail.com>
2025-01-25 12:28:23 -0500

    auth done

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,5 +1,5 @@
 import {MongoClient} from "mongodb";
-const uri = process.env.MONGO;
+const uri = process.env.MONG;
 const client=new MongoClient(uri)
 const clientPromise=client.connect()
 export default clientPromise
